6f6b341c9084fd3f3dcbd3aae154d0ef54a61a9d
Diary <raharimanantenadiary@gmail.com>
2024-04-07 15:43:05 +0300

    analyse d'un post tout simple

---
+++ src/main/java/com/mycompany/tp2raharimanantenadiary/config/ConfigJSF.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.tp2raharimanantenadiary.config;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.faces.annotation.FacesConfig;
+/**
+ * Configuration JSF
+ * @author Diary
+ */
+@ApplicationScoped
+@FacesConfig
+public class ConfigJSF {
+    
+}
